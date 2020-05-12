ac86901c1b69ced78a20ad62a97e6ca04ede343b
HappyRyan2 <happyryan2@hotmail.com>
2020-05-12 08:18:49 -0700

    added project description text + hover effects

---
+++ hoverEffects.js
@@ -0,0 +1,27 @@
+function addHoverEffect(containerID) {
+	var container = document.getElementById(containerID);
+
+	var linkElement = container.children[0];
+	var image = linkElement.children[0];
+	var text = linkElement.children[1];
+
+	var hovering = false;
+	container.onmouseover = function() {
+		image.style["-webkit-filter"] = "blur(50px)";
+		container.style["z-index"] = "1000";
+		image.style["z-index"] = "1000";
+		text.style.opacity = "1";
+	};
+	container.onmouseout = function() {
+		image.style["-webkit-filter"] = "blur(0px)";
+		container.style["z-index"] = "0";
+		image.style["z-index"] = "0";
+		text.style.opacity = "0";
+	};
+};
+window.onload = function() {
+	addHoverEffect("random-survival-game");
+	addHoverEffect("stick-dungeon");
+	addHoverEffect("hypocube-translocation");
+	addHoverEffect("raycaster");
+};
