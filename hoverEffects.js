98941fd5690aa3d2e3c2839eba08d6c9f7bba005
HappyRyan2 <HappyRyan2@Hotmail.com>
2025-05-04 12:57:27 -0700

    changed hover effects to click/tap effects

--- hoverEffects.js
+++ hoverEffects.js
@@ -1,22 +1,44 @@
+const PROJECTS = [
+	"random-survival-game",
+	"stick-dungeon",
+	"hypocube-translocation",
+	"raycaster"
+];
+
+let dialogShown = null;
+
+function openDialog(containerID) {
+	const container = document.getElementById(containerID);
+	const image = container.querySelector("img");
+	const text = container.querySelector(".project-text");
+	image.style["-webkit-filter"] = "blur(50px)";
+	container.style["z-index"] = "1";
+	image.style["z-index"] = "1";
+	text.style.opacity = "1";
+}
+function closeDialog(containerID) {
+	const container = document.getElementById(containerID);
+	const image = container.querySelector("img");
+	const text = container.querySelector(".project-text");
+	image.style["-webkit-filter"] = "blur(0px)";
+	container.style["z-index"] = "0";
+	image.style["z-index"] = "0";
+	text.style.opacity = "0";
+}
+
 function addHoverEffect(containerID) {
-	var container = document.getElementById(containerID);
+	const container = document.getElementById(containerID);
+	container.onclick = function() {
+		for(const id of PROJECTS) {
+			closeDialog(id);
+		}
+		if(dialogShown === containerID) {
 
-	var linkElement = container.children[0];
-	var image = linkElement.children[0];
-	var text = linkElement.children[1];
-
-	var hovering = false;
-	container.onmouseover = function() {
-		image.style["-webkit-filter"] = "blur(50px)";
-		container.style["z-index"] = "1";
-		image.style["z-index"] = "1";
-		text.style.opacity = "1";
-	};
-	container.onmouseout = function() {
-		image.style["-webkit-filter"] = "blur(0px)";
-		container.style["z-index"] = "0";
-		image.style["z-index"] = "0";
-		text.style.opacity = "0";
+		}
+		else {
+			openDialog(containerID);
+			dialogShown  = containerID;
+		}
 	};
 };
 window.onload = function() {
