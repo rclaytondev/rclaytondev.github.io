515072cc3b08f27f468bb1bb26f8357e72124e86
HappyRyan2 <HappyRyan2@Hotmail.com>
2026-07-17 12:59:54 -0700

    added custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Oxygen } from "next/font/google";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
+const oxygen = Oxygen({
+	weight: ["300", "400"],
+	variable: "--font-oxygen",
+	subsets: ["latin"]
+})
 
 export const metadata: Metadata = {
 	title: "My Projects",
@@ -24,7 +20,7 @@ export default function RootLayout({
 	return (
 		<html
 			lang="en"
-			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+			className={`${oxygen.variable} h-full antialiased`}
 		>
 			<body className="min-h-full flex flex-col">{children}</body>
 		</html>
