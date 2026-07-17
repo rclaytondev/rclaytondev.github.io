abd317fbad0cac30a7f4f11997d101ce82279964
HappyRyan2 <HappyRyan2@Hotmail.com>
2026-07-17 15:56:24 -0700

    removed basePath due to site URL change

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
-const REPOSITORY_NAME = "rclaytondev.github.io-new";
-const isProd = process.env.NODE_ENV === 'production';
-const prefix = isProd ? `/${REPOSITORY_NAME}` : "";
-
 const nextConfig: NextConfig = {
 	/* config options here */
 	"output": "export",
-	basePath: prefix,
-	assetPrefix: prefix,
 };
 
 export default nextConfig;
